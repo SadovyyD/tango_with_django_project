e20c77d2ddf380318a6a174b2ff0455092614b54
Danylo Sadovyy <3026466s@student.gla.ac.uk>
2026-02-07 17:31:32 +0000

    added category form support

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -12,9 +13,11 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
 
     return render(request, 'rango/about.html')
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -28,5 +31,19 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+              print(form.errors)
+    return render(request, 'rango/add_category.html', {'form':form})
+
+    
 
 
